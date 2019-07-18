edd85fcf4751a59cf3fdfe5c8eeccfe44281f4ce
Keith Murphy <kmurphy@catalystsolves.com>
2019-07-18 16:19:40 -0700

    master: Setting up the header part of the site and gathered assets that will be needed.

--- api/routes.js
+++ api/routes.js
@@ -5,4 +5,8 @@ Router.get('/hello-world', (req, res) => {
    res.send({testing: 'This is a string that we are sending'});
 });
 
+Router.get('/hello-world-2', (req, res) => {
+   res.status(404);
+});
+
 module.exports = Router;
